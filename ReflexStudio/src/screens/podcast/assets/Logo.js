40a58d1a5e87fcff540b568c29b4b3da2a36691e
Ahmed <ahmedali.96@hotmail.com>
2020-03-01 17:31:13 -0500

    deleted redundant files

--- ReflexStudio/src/screens/podcast/assets/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {Image} from 'react-native';
-
-export default Logo = () => {
-  return (
-    <Image source={require('./Logo.png')} style={{height: 130, width: 300}} />
-  );
-};
