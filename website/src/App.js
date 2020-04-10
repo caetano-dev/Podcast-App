0b6b59204316af8859018747ffc2ead9ec781825
Ahmed <ahmedali.96@hotmail.com>
2020-04-10 09:55:12 -0400

    reflexstudios.ca now live via firebase hosting

---
+++ website/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          If you're reading this ReflexStudios.ca is still under construction
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
