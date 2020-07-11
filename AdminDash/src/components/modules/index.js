9ef2e5440b85bdab7acdce0b737c3a06a5923464
Ahmed <ahmedali.96@hotmail.com>
2020-07-11 18:48:29 -0400

    removed unrelated folders

--- AdminDash/src/components/modules/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Placeholder } from "./Placeholder";
-export { default as SlangCard } from "./SlangCard";
-export { default as SlangModal } from "./SlangModal";
-export { default as SlangChart } from "./SlangChart";
